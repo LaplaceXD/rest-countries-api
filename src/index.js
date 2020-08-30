60d91ae1d0065e3d6d3b3aa1146b611d3862986a
Laplace <johnalexisbuot@gmail.com>
2020-08-30 16:48:31 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./sass/main.scss";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const PATH = require("path");
+const HTML_WEBPACK_PLUGIN = require("html-webpack-plugin");
+const BABEL_MINIFY_WEBPACK_PLUGIN = require("babel-minify-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.[hash].js",
+        path: PATH.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        modules: [__dirname, "src", "node_modules"],
+        extensions: ["*", ".js", ".jsx"],
+    },
+    module: {
+        rules: [
+            { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
+            {
+                test: /\.s?css$/,
+                exclude: /node_modules/,
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+            },
+            { test: /\.html$/, use: "html-loader" },
+            { test: /\.(png|gif|jpe?g|svg)$/, use: "file-loader" },
+        ],
+    },
+    plugins: [
+        new HTML_WEBPACK_PLUGIN({
+            template: "./src/index.html",
+            filename: "./index.html",
+        }),
+        new BABEL_MINIFY_WEBPACK_PLUGIN(
+            {},
+            {
+                comments: false,
+            }
+        ),
+    ],
+};
