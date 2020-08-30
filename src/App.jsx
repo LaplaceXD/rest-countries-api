8a06deee77c3dcfb37f599290a53803332600d1f
Laplace <johnalexisbuot@gmail.com>
2020-08-30 20:44:42 +0800

    Added search box functionality and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import http from "./services/httpService";
 import Card from "./components/common/Card";
+import { filterByString } from "./utils/filterMethods";
 
 const apiEndpoint = "https://restcountries.eu/rest/v2/all";
 
 function App() {
     const [countries, setCountries] = useState([]);
+    const [search, setSearch] = useState("");
+    const filteredCountries = filterByString(countries, "name", search);
 
     useEffect(() => {
         async function getCountries() {
@@ -19,8 +22,16 @@ function App() {
     return (
         <div className="main">
             <main className="container">
-                <section className="l-grid container">
-                    {countries.map((country) => (
+                <section className="l-flex">
+                    <input
+                        type="text"
+                        placeholder="Search for a country..."
+                        className="search-box"
+                        onChange={(e) => setSearch(e.currentTarget.value)}
+                    />
+                </section>
+                <section className="l-grid country-container">
+                    {filteredCountries.map((country) => (
                         <Card
                             key={country.alpha3Code}
                             display={["population", "region", "capital"]}
