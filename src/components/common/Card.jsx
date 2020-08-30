885e91e668df75ad8af1fd9684502367cf85a515
Laplace <johnalexisbuot@gmail.com>
2020-08-30 18:14:05 +0800

    Added Card Template

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React from "react";
 
-function Card() {
-    return;
+function Card({ country }) {
+    const { name, flag, population, region, capital } = country;
+
+    return (
+        <div>
+            <img src={flag} alt={`Flag of ${name}`} />
+            <h1>{name}</h1>
+            <ul>
+                <li>
+                    <span>Population:</span>
+                    {population}
+                </li>
+                <li>
+                    <span>Region:</span>
+                    {region}
+                </li>
+                <li>
+                    <span>Capital:</span>
+                    {capital}
+                </li>
+            </ul>
+        </div>
+    );
 }
+
+export default Card;
