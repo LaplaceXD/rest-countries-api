05c550bae87a68a1ab2852f4e663602288c7a652
Laplace <johnalexisbuot@gmail.com>
2020-09-01 07:25:12 +0800

    Refactored filter methods for readability

--- src/utils/filterMethods.js
+++ src/utils/filterMethods.js
@@ -1,23 +1,23 @@
-export function filterByKeys(objToFilter, keyFilter) {
-    return Object.keys(objToFilter)
-        .filter((key) => keyFilter.includes(key))
-        .reduce((details, key) => {
-            const newObj = {
-                key: key,
-                value: objToFilter[key],
-            };
+export function filterByKeys(objToFilter, wantedKeys) {
+    const availableKeys = Object.keys(objToFilter).filter((key) => wantedKeys.includes(key));
+    const filtered = availableKeys.reduce((items, key) => {
+        items.push({
+            key: key,
+            value: objToFilter[key],
+        });
 
-            details.push(newObj);
-            return details;
-        }, []);
+        return items;
+    }, []);
+
+    return filtered;
 }
 
 export function filterByString(obj, key, searchString) {
-    return obj.filter((country) => country[key].toLowerCase().startsWith(searchString.toLowerCase()));
+    return obj.filter((item) => item[key].toLowerCase().startsWith(searchString.toLowerCase()));
 }
 
 export function filterByCriteria(obj, key, criteria) {
     if (!criteria) return obj;
 
-    return obj.filter((obj) => obj[key] === criteria);
+    return obj.filter((item) => item[key] === criteria);
 }
