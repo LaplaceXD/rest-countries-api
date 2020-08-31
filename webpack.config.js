0d9396d55608c0cc669a42b20f73f506f46ac5d7
Laplace <johnalexisbuot@gmail.com>
2020-08-31 21:21:30 +0800

    Removed incompatible dependency

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const PATH = require("path");
 const WEBPACK = require("webpack");
 const HTML_WEBPACK_PLUGIN = require("html-webpack-plugin");
 const BABEL_MINIFY_WEBPACK_PLUGIN = require("babel-minify-webpack-plugin");
-const MINI_CSS_EXTRACT_PLUGIN = require("mini-css-extract-plugin");
 const AUTOPREFIXER = require("autoprefixer");
 
 module.exports = {
@@ -21,7 +20,7 @@ module.exports = {
             {
                 test: /\.s?css$/,
                 exclude: /node_modules/,
-                use: [MINI_CSS_EXTRACT_PLUGIN.loader, "css-loader", "postcss-loader", "sass-loader"],
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
             },
             { test: /\.html$/, use: "html-loader" },
             { test: /\.(png|gif|jpe?g|svg)$/, use: "file-loader" },
@@ -42,10 +41,6 @@ module.exports = {
             options: {
                 postcss: [AUTOPREFIXER()],
             },
-        }),
-        new MINI_CSS_EXTRACT_PLUGIN({
-            filename: "[name].[contenthash].css",
-            chunkFilename: "[name].[contenthash].css",
         }),
     ],
 };
