465426fb440f6bc517fe7c084d6cb2e8eae5cc12
Laplace <johnalexisbuot@gmail.com>
2020-09-02 10:29:03 +0800

    Refactored error handling in loadingCountries

--- src/components/hooks/useGetCountries.js
+++ src/components/hooks/useGetCountries.js
@@ -1,27 +1,13 @@
 import { useEffect, useState } from "react";
-import { toast } from 'react-toastify';
 import { getCountries } from "../../services/countriesService";
+import error from "../../services/errorService";
 
 async function loadCountries(fields, setCallback) {
     try {
         const { data } = await getCountries(fields);
         setCallback(data);
     } catch (ex) {
-        const {
-            response,
-            response: { status },
-        } = ex;
-
-        if (response) {
-            switch (status) {
-                case "400":
-                    toast.error("Bad Request");
-                    break;
-                case "404":
-                    toast.error("Page not Found");
-                    break;
-            }
-        }
+        error.handle(ex);
     }
 }
 
---
+++ src/services/errorService.js
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+
+function handleExpectedError(error) {
+    const {
+        response,
+        response: { status },
+    } = error;
+
+    if (response) {
+        switch (status) {
+            case "400":
+                toast.error("Bad Request");
+                break;
+            case "404":
+                toast.error("Page not Found");
+                break;
+            default:
+                toast.error("An error occured, please try again!");
+                break;
+        }
+    }
+
+    toast.error("Failed to fetch the data from the server.");
+}
+
+export default {
+    handle: handleExpectedError,
+}
