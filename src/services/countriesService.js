35d0dd1492c06e5ee4983000080b434756844931
Laplace <johnalexisbuot@gmail.com>
2020-09-02 10:13:08 +0800

    Added getcountry function for preview page

--- src/services/countriesService.js
+++ src/services/countriesService.js
@@ -2,12 +2,26 @@ import http from "./httpService";
 import config from "./config.json";
 
 const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+const { apiEndpoint } = config;
+
+function createQueryString(fields) {
+    return fields.reduce((query, field) => query + field + ";", "?fields=");
+}
 
 export function getCountries(fields) {
-    if(!fields) return http.get(config.apiEndpoint);
+    if(!fields) return http.get(apiEndpoint);
 
-    const queryString = fields.reduce((query, field) => query + field + ";", "?fields=");
-    return http.get(config.apiEndpoint + queryString);
+    const queryString = createQueryString(fields);
+    return http.get(apiEndpoint + queryString);
+}
+
+export function getCountry(name, fields) {
+    const apiNameEndpoint = `${apiEndpoint}/name/${name}`;
+
+    if(!fields) return http.get(apiNameEndpoint)
+
+    const queryString = createQueryString(fields);
+    return http.get(apiNameEndpoint + queryString);
 }
 
 export function getRegions() {
