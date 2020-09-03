93b0561240bd26bc9dbb5252533cb398cf9f6437
Laplace <johnalexisbuot@gmail.com>
2020-09-03 12:44:40 +0800

    Production configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const PATH = require("path");
 const WEBPACK = require("webpack");
 const HTML_WEBPACK_PLUGIN = require("html-webpack-plugin");
-// const MINI_CSS_EXTRACT_PLUGIN = require("mini-css-extract-plugin");
+const MINI_CSS_EXTRACT_PLUGIN = require("mini-css-extract-plugin");
 const BABEL_MINIFY_WEBPACK_PLUGIN = require("babel-minify-webpack-plugin");
 const AUTOPREFIXER = require("autoprefixer");
 
@@ -23,7 +23,7 @@ module.exports = {
             {
                 test: /\.s?css$/,
                 exclude: /node_modules/,
-                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+                use: [MINI_CSS_EXTRACT_PLUGIN.loader, "css-loader", "postcss-loader", "sass-loader"],
             },
             { test: /\.html$/, use: "html-loader" },
             { test: /\.(png|gif|jpe?g|svg)$/, use: "file-loader" },
@@ -37,10 +37,10 @@ module.exports = {
             template: "./src/index.html",
             filename: "./index.html",
         }),
-        // new MINI_CSS_EXTRACT_PLUGIN({
-        //     filename: "[name].[contenthash].css",
-        //     chunkFilename: "[name].[contenthash].css",
-        // }),
+        new MINI_CSS_EXTRACT_PLUGIN({
+            filename: "[name].[contenthash].css",
+            chunkFilename: "[name].[contenthash].css",
+        }),
         new BABEL_MINIFY_WEBPACK_PLUGIN(
             {},
             {
